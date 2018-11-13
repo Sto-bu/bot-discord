c8c2f90f24bb5382b6b4f5493d8c127af058f8e8
Sto-bu <44979019+Sto-bu@users.noreply.github.com>
2018-11-13 18:56:22 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
  const client = new Discord.Client();
  var prefix = "*";
  
- client.login(conv.env.TOKEN)
+ client.login(process.env.TOKEN)
 
  client.on('message', async message => { 
 
