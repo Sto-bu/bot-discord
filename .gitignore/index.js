4371113dac649a854d4b87254079eb16f71a5e20
Sto-bu <44979019+Sto-bu@users.noreply.github.com>
2018-11-13 18:47:40 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+ const client = new Discord.Client();
+ var prefix = "*";
+ 
+ client.login(conv.env.TOKEN)
+
+ client.on('message', async message => { 
+
+    if(message.content === prefix + "Bonjour"){
+        message.reply("Yo");
+        console.log('Le bot dit bonjour');
+    }
+
+});
